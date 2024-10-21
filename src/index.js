23a950d26eac2dc871bb287c0167afe2d4e2034e
zafle <fleutotanne@free.fr>
2024-10-21 15:26:39 +0200

    add prettier config

--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import reportWebVitals from './reportWebVitals'
 import Header from './layout/Header'
 import Home from './pages/Home'
 import About from './pages/About'
 import Error404 from './pages/Error404'
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </Router>
   </React.StrictMode>
 )
 
-
-
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
