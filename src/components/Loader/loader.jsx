f5e8f234346bf4c8d1c0129107e33833b25fffa9
zafle <fleutotanne@free.fr>
2024-11-04 10:37:21 +0100

    fix map method in loader

--- src/components/Loader/loader.jsx
+++ src/components/Loader/loader.jsx
@@ -4,7 +4,7 @@ function Loader() {
   return (
     <div className="loader">
       <div className="loader__content">
-        {[...Array(12)].map((index) => (
+        {[...Array(12)].map((element, index) => (
           <div key={`loader-${index}`}></div>
         ))}
       </div>
